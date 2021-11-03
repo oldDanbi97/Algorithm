db9fbba95c063b53d4d70a9e8914733933008f89
minju <49747442+minjae2021@users.noreply.github.com>
2021-11-03 16:02:45 +0900

    튜플

---
+++ 2019_카카오_개발자_겨울_인턴십/튜플.py
@@ -0,0 +1,9 @@
+def solution(s):
+    tuple_list = [i.split(',') for i in s[1:-2].replace('{','').split('},')]
+    tuple_list = sorted(tuple_list, key= lambda x: len(x))
+    tmp_tupe = tuple_list[0]
+    for i in tuple_list[1:]:
+        if set(tmp_tupe) != set(i):
+            for j in [k for k in i if k not in tmp_tupe]:
+                tmp_tupe.append(j)
+    return list(map(int, tmp_tupe))
