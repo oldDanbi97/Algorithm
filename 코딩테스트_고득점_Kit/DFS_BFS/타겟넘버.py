7d8bb1a93485b31d9bcf46f8adfd78b57726fe6f
minju <49747442+minjae2021@users.noreply.github.com>
2021-11-23 15:40:35 +0900

    타겟넘버(DFS)

--- 코딩테스트_고득점_Kit/DFS_BFS/타겟넘버.py
+++ 코딩테스트_고득점_Kit/DFS_BFS/타겟넘버.py
@@ -1,3 +1,18 @@
+answer = 0
+arr_len = 0
+def dfs(numbers, target, idx, result):
+    global answer
+    if idx == arr_len:
+        if target == result:
+            answer += 1
+        return
+    else:
+        dfs(numbers, target, idx+1, result + numbers[idx])
+        dfs(numbers, target, idx+1, result - numbers[idx])
+
 def solution(numbers, target):
-    answer = 0
+    global answer
+    global arr_len
+    arr_len = len(numbers)
+    dfs(numbers, target, 0, 0)
     return answer
