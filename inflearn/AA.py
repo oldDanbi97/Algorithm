89c14211ac30a32a5d47d95082d1574661aac8f1
minju <49747442+minjae2021@users.noreply.github.com>
2022-01-05 18:50:52 +0900

    리스트

--- inflearn/AA.py
+++ inflearn/AA.py
@@ -1,13 +1,27 @@
 import sys
+from collections import deque
 sys.stdin=open(r"./inflearn/input.txt", "rt")
-N = int(input())
-answer = list(map(int, input().split()))
+n = int(input())
+a = [deque(map(int,input().split())) for _ in range(n)]
+m = int(input())
+b = [list(map(int,input().split())) for _ in range(m)]
+for i in b:
+    l, d, nn = i
+    if d == 0:
+        for _ in range(nn):
+            a[l-1].append(a[l-1].popleft())
+    else:
+        for _ in range(nn):
+            a[l-1].appendleft(a[l-1].pop())
 res = 0
-plus = 1
-for i in answer:
-    if i == 1:
-        res += plus
-        plus += 1
-    elif i == 0:
-        plus = 1
+s, e = 0, n-1
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n//2:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
 print(res)
